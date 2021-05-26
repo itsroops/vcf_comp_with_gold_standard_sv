dc6c68f657c0b80aab291c51975d95cb4d53010f
itsroops <46230392+itsroops@users.noreply.github.com>
2021-05-26 16:23:06 +0200

    Add files via upload

--- vcf_comppp.sh
+++ vcf_comppp.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
 # Calling the truvari tool for comaparisons between gold standard file and other vcf files
-$1/truvari bench -b $2 -c $3 -f $4 -o $5 -r $6
+$1/truvari bench -b $2 -c $3 -f $5 -o $4
